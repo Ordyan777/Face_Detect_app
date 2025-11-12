812c697f09bdd211d1a0c3ecae5f25f627d66446
DavOrdyan <83502954+Ordyan777@users.noreply.github.com>
2025-11-12 16:44:44 +0400

    Add files via upload

---
+++ venv_test.py
@@ -0,0 +1,12 @@
+import cv2
+import face_recognition
+import numpy
+import pickle
+import json
+import sys, site
+print(site.getsitepackages())
+
+
+
+print("Environment ready!")
+print(" ---=== Ordyan777 ===--- ")
